9469d33a0450984a2f678e489820e51bf42b56d6
Mathis Neunzig <mathis.neunzig@sap.com>
2022-12-31 18:23:35 +0100

    skills tile

--- nachhilfe/src/components/tiles/Skills.tsx
+++ nachhilfe/src/components/tiles/Skills.tsx
@@ -1,21 +1,196 @@
 import React from 'react';
-import { Avatar, VStack, Box, Flex, Center } from 'native-base';
+import { Alert, HStack, VStack, Skeleton, Box } from 'native-base';
 
 export const Example = () => {
 	return (
-		<VStack justifyContent="center" w="80%">
-            <Avatar
-                alignSelf="center"
-                size="xl"
-                _light={{ bg: 'red.300' }}
-                _dark={{ bg: 'red.400' }}
-                _text={{ opacity: 30 }}
-                source={{ uri: "https://thumbs.dreamstime.com/b/red-human-brain-icon-stock-vector-red-human-brain-icon-vector-117693464.jpg" }}
+        <>
+            <HStack
+                space={3}
+                alignItems="center"
+                m={4}
+                p={2}
+                rounded="md"
+                _light={{
+                    bg: 'error.100',
+                }}
+                _dark={{
+                    bg: 'error.400',
+                }}
+                w="70%"
             >
+            <VStack space={1} flex={1}>
+                <Box
+                    _light={{
+                        bg: 'red.400',
+                    }}
+                    _dark={{
+                        bg: 'gray.100',
+                    }}
+                    rounded="sm"
+                    h="11px"
+                    w="50%"
+                />
+                <Box
+                    _light={{
+                        bg: 'red.300',
+                    }}
+                    _dark={{
+                        bg: 'gray.100',
+                    }}
+                    h="8px"
+                    rounded="pill"
+                    w="100%"
+                />
+                <Box
+                    _light={{
+                        bg: 'red.0',
+                    }}
+                    _dark={{
+                        bg: 'gray.0',
+                    }}
+                    h="8px"
+                    rounded="pill"
+                    w="100%"
+                />
+                <Box
+                    _light={{
+                        bg: 'red.400',
+                    }}
+                    _dark={{
+                        bg: 'gray.100',
+                    }}
+                    rounded="sm"
+                    h="11px"
+                    w="50%"
+                />
+                <Box
+                    _light={{
+                        bg: 'red.300',
+                    }}
+                    _dark={{
+                        bg: 'gray.100',
+                    }}
+                    h="8px"
+                    rounded="pill"
+                    w="100%"
+                />
+            </VStack>
+                <VStack space={1} flex={1}>
+                    <Box
+                        _light={{
+                            bg: 'red.400',
+                        }}
+                        _dark={{
+                            bg: 'gray.100',
+                        }}
+                        rounded="sm"
+                        h="11px"
+                        w="50%"
+                    />
+                    <Box
+                        _light={{
+                            bg: 'red.300',
+                        }}
+                        _dark={{
+                            bg: 'gray.100',
+                        }}
+                        h="8px"
+                        rounded="pill"
+                        w="100%"
+                    />
+                    <Box
+                        _light={{
+                            bg: 'red.0',
+                        }}
+                        _dark={{
+                            bg: 'gray.0',
+                        }}
+                        h="8px"
+                        rounded="pill"
+                        w="100%"
+                    />
+                    <Box
+                        _light={{
+                            bg: 'red.400',
+                        }}
+                        _dark={{
+                            bg: 'gray.100',
+                        }}
+                        rounded="sm"
+                        h="11px"
+                        w="50%"
+                    />
+                    <Box
+                        _light={{
+                            bg: 'red.300',
+                        }}
+                        _dark={{
+                            bg: 'gray.100',
+                        }}
+                        h="8px"
+                        rounded="pill"
+                        w="100%"
+                    />
+                </VStack>
+                <VStack space={1} flex={1}>
+                    <Box
+                        _light={{
+                            bg: 'red.400',
+                        }}
+                        _dark={{
+                            bg: 'gray.100',
+                        }}
+                        rounded="sm"
+                        h="11px"
+                        w="50%"
+                    />
+                    <Box
+                        _light={{
+                            bg: 'red.300',
+                        }}
+                        _dark={{
+                            bg: 'gray.100',
+                        }}
+                        h="8px"
+                        rounded="pill"
+                        w="100%"
+                    />
+                    <Box
+                        _light={{
+                            bg: 'red.0',
+                        }}
+                        _dark={{
+                            bg: 'gray.0',
+                        }}
+                        h="8px"
+                        rounded="pill"
+                        w="100%"
+                    />
+                    <Box
+                        _light={{
+                            bg: 'red.400',
+                        }}
+                        _dark={{
+                            bg: 'gray.100',
+                        }}
+                        rounded="sm"
+                        h="11px"
+                        w="50%"
+                    />
+                    <Box
+                        _light={{
+                            bg: 'red.300',
+                        }}
+                        _dark={{
+                            bg: 'gray.100',
+                        }}
+                        h="8px"
+                        rounded="pill"
+                        w="100%"
+                    />
+                </VStack>
+            </HStack>
+        </>
 
-
-
-            </Avatar>
-		</VStack>
 	);
 };
